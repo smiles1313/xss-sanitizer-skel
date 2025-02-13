684062ebe68323d33b2403089c6469652ed3fc98
smiles1313 <justs.miles1313@gmail.com>
2025-02-12 19:51:41 -0800

    made tester

---
+++ sanitizer/tester.mjs
@@ -0,0 +1,23 @@
+const htmlRender = document.getElementById("rendered");
+const c = await fetch("test_cases.txt")
+let c_text = await c.text()
+import { Sanitizer } from './sanitize.js'
+
+const cleaned = Sanitizer.sanitize(c_text);
+
+c_text = "<script>function alert(text) { parent.postMessage('test') }</script>" + c_text
+
+console.log(cleaned)
+htmlRender.setAttribute("srcdoc", cleaned);
+
+window.addEventListener(
+    "message",
+    (event) => {
+        if (event.origin !== window.location.origin) {
+            return;
+        }
+        console.log(event)
+    },
+    false,
+  );
+  
