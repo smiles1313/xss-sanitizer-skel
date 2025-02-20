973699d33f3a7c7f3fde67c02e09bc4c2b5e3716
Aplet123 <aplet@aplet.me>
2025-02-19 19:31:19 -0800

    fix module for index.js

--- sanitizer/index.js
+++ sanitizer/index.js
@@ -1,3 +1,5 @@
+import { Sanitizer } from "./sanitize.js";
+
 const sanitizeButton = document.getElementById("sanitize");
 const htmlInput = document.getElementById("input");
 const htmlOutput = document.getElementById("output");
